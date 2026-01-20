f480b88919aae0a573f4773da1c20e066545fc8b
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2026-01-20 00:33:30 +0000

    cambios en el navbar

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand text-light" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link text-white-50" href="#">About</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
+                        <a className="nav-link text-white-50" href="#">Services</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <a className="nav-link text-white-50" href="#">Contact</a>
                     </li>
                 </ul>
                 </div>
